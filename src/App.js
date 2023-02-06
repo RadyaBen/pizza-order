1e4c7acb2c8f93f092e39f84668deb98e17cafa7
RadyaBen <lizoguben.radya@gmail.com>
2023-02-06 18:13:01 +0200

    Add a styled <Header> component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Header } from './components';
+
 import './scss/app.scss';
 
 export const App = () => {
 	return (
-		<div>
-			Hello World!
+		<div className='wrapper'>
+			<Header />
 		</div>
 	);
 };
