80ae0829cb6c6cd5ef5013b19ad6278bc6c03f37
RadyaBen <lizoguben.radya@gmail.com>
2023-08-08 12:26:50 +0300

    Transform SVGs into components

---
+++ src/components/ui/icons/ArrowBackIcon.tsx
@@ -0,0 +1,17 @@
+export const ArrowBackIcon = () => (
+	<svg
+		width='8'
+		height='14'
+		viewBox='0 0 8 14'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<path
+			d='M7 13L1 6.93015L6.86175 1'
+			stroke='#868484'
+			strokeWidth='1.5'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		/>
+	</svg>
+);
